f764a36ca5871347c3af861a4b6282adebcc4a6d
Александр <@DESKTOP-L9V170T>
2025-05-27 22:07:09 +0300

    Добавлена сущность пользователей и организаций

---
+++ Lib/DigitTwin.Lib.Abstractions/Enums/UserTypeEnum.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DigitTwin.Lib.Abstractions
+{
+    /// <summary>
+    /// Типы пользователей
+    /// </summary>
+    public enum UserTypeEnum
+    {
+        [Display(Name = "Система")]
+        SYSTEM = 0,
+
+        [Display(Name = "Пользователь")]
+        USER = 1,
+
+        [Display(Name = "Администратор")]
+        ADMINISTRATOR = 2
+    }
+}
