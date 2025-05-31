54161aa9c3d74adc1cf2567feda05ef52a2bf972
Александр <@DESKTOP-L9V170T>
2025-05-31 22:45:52 +0300

    Перестроение структуры проекта

--- Core/Users/DigitTwin.Core.Users.Abstractions/Filters/GetSingleUserFilter.cs
+++
@@ -1,9 +0,0 @@
-﻿using DigitTwin.Lib.Abstractions.Filters;
-
-namespace DigitTwin.Core.Users.Abstractions.Filters
-{
-    public class GetSingleUserFilter : IBaseFilter
-    {
-        public Guid Id { get; set; }
-    }
-}
