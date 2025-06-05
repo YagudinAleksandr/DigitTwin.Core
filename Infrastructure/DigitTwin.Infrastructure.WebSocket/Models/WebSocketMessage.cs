efd80fc19ee4ad523d5ae28175a6850ab8ba133f
Александр <@DESKTOP-L9V170T>
2025-06-05 22:45:18 +0300

    Добавление библиотеки WebSocket

---
+++ Infrastructure/DigitTwin.Infrastructure.WebSocket/Models/WebSocketMessage.cs
@@ -0,0 +1,18 @@
+﻿namespace DigitTwin.Infrastructure.WS
+{
+    /// <summary>
+    /// Содель сообщения для WebSocket
+    /// </summary>
+    public class WebSocketMessage
+    {
+        /// <summary>
+        /// Тип сообщения
+        /// </summary>
+        public string MessageType { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Сообщение
+        /// </summary>
+        public string Data { get; set; } = string.Empty;
+    }
+}
---
+++ Infrastructure/DigitTwin.Infrastructure.WebSocket/Services/WebSocketConnectionManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+
+namespace DigitTwin.Infrastructure.WS
+{
+    /// <inheritdoc cref="IWebSocketConnectionManager"/>
+    internal class WebSocketConnectionManager : IWebSocketConnectionManager
+    {
+        /// <summary>
+        /// Список соединений
+        /// </summary>
+        private readonly ConcurrentDictionary<string, WebSocket> _sockets = new ConcurrentDictionary<string, WebSocket>();
+
+        public void AddSocket(WebSocket socket, string id)
+        {
+            _sockets.TryAdd(id, socket);
+        }
+
+        public IEnumerable<WebSocket> GetAllSockets()
+        {
+            return _sockets.Values;
+        }
+
+        public string GetId(WebSocket socket)
+        {
+            return _sockets.FirstOrDefault(x => x.Value == socket).Key;
+        }
+
+        public WebSocket GetSocketById(string id)
+        {
+            _sockets.TryGetValue(id, out var socket);
+            return socket;
+        }
+
+        public void RemoveSocket(string id)
+        {
+            _sockets.TryRemove(id, out _);
+        }
+    }
+}
