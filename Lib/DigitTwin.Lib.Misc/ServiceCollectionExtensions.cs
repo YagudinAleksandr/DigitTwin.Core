2aa4f5a277993ccdab3f746ae167dce8f0cc6d40
a.yagudin <a.yagudin@DEV-01>
2025-06-09 16:36:44 +0300

    Добавление библиотеки для пайплайнов

--- Lib/DigitTwin.Lib.Misc/ServiceCollectionExtensions.cs
+++ Lib/DigitTwin.Lib.Misc/ServiceCollectionExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using DigitTwin.Lib.EventBroker;
+using DigitTwin.Lib.PipelineBuilder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace DigitTwin.Lib.Misc
 {
@@ -15,6 +17,48 @@ namespace DigitTwin.Lib.Misc
         public static IServiceCollection AddMiscServices(this IServiceCollection services, IConfiguration configuration) 
         {
             services.AddSingleton<IEventBroker, EventBroker.EventBroker>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Регистрирует синхронный и асинхронный пайплайны с шагами из указанных сборок
+        /// </summary>
+        /// <typeparam name="TContext">Тип контекста обработки</typeparam>
+        /// <param name="services">Коллекция сервисов</param>
+        /// <param name="assemblies">Сборки для поиска шагов</param>
+        public static IServiceCollection AddPipelines<TContext>(this IServiceCollection services, params Assembly[] assemblies)
+        {
+            if (assemblies.Length == 0)
+                assemblies = [Assembly.GetExecutingAssembly()];
+
+            // Регистрация ВСЕХ синхронных шагов
+            var syncStepTypes = assemblies
+                .SelectMany(a => a.GetTypes())
+                .Where(t => typeof(IPipelineStep<TContext>).IsAssignableFrom(t)
+                            && !t.IsInterface
+                            && !t.IsAbstract);
+
+            foreach (var type in syncStepTypes)
+            {
+                services.AddTransient(typeof(IPipelineStep<TContext>), type);
+            }
+
+            // Регистрация ВСЕХ асинхронных шагов
+            var asyncStepTypes = assemblies
+                .SelectMany(a => a.GetTypes())
+                .Where(t => typeof(IAsyncPipelineStep<TContext>).IsAssignableFrom(t)
+                            && !t.IsInterface
+                            && !t.IsAbstract);
+
+            foreach (var type in asyncStepTypes)
+            {
+                services.AddTransient(typeof(IAsyncPipelineStep<TContext>), type);
+            }
+
+            // Регистрация самого пайплайна
+            services.AddTransient<IPipeline<TContext>, Pipeline<TContext>>();
+            services.AddTransient<IAsyncPipeline<TContext>, AsyncPipeline<TContext>>();
 
             return services;
         }
