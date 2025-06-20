c399dc2275d9cee05f353f08d60d99fab7328c6d
a.yagudin <a.yagudin@DEV-01>
2025-06-20 17:06:25 +0300

    Добавлена связка с пользователями и организациями

---
+++ Core/Users/DigitTwin.Core.Users.Logic/Data/Entities/OrganizationUser.cs
@@ -0,0 +1,27 @@
+﻿using DigitTwin.Lib.Abstractions;
+
+namespace DigitTwin.Core.Users.Logic.Data.Entities
+{
+    /// <summary>
+    /// Связь пользователей с организацией
+    /// </summary>
+    public class OrganizationUser : IEntity<Guid>
+    {
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// ИД пользователя
+        /// </summary>
+        public Guid UserId { get; set; }
+
+        /// <summary>
+        /// ИД организации
+        /// </summary>
+        public Guid OrganizationId { get; set; }
+
+        /// <summary>
+        /// Тип пользователя
+        /// </summary>
+        public OrganizationUsersTypeEnum UsersType { get; set; }
+    }
+}
