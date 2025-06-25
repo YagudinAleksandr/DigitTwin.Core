6f948e4816b70c0efd295b8c039e863f04fd7774
Александр <@DESKTOP-L9V170T>
2025-06-25 23:56:27 +0300

    Полностью законченая сборка для MongoDB

---
+++ Infrastructure/DigitTwin.Infrastructure.Mongo/Services/IMongoDbService.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Driver;
+
+namespace DigitTwin.Infrastructure.Mongo
+{
+    /// <summary>
+    /// Сервис для работы с MongoDB
+    /// </summary>
+    public interface IMongoDbService
+    {
+        /// <summary>
+        /// База MongoDB
+        /// </summary>
+        IMongoDatabase Database { get; }
+
+        /// <summary>
+        /// Клиент
+        /// </summary>
+        IMongoClient Client { get; }
+    }
+}
